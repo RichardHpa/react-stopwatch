c2525a51f2d483af4484eebdcd6c0373a45733b8
RichardHpa <richard.hpa@hotmail.com>
2019-05-02 21:35:43 +1200

    edited stopwatch to include radial timer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 var timerInt;
 
 class App extends Component {
@@ -9,11 +9,12 @@ class App extends Component {
             timerOn: false,
             timerDuration: 0,
             buttonText: 'Start Stop Watch',
-            buttonClass: 'btn btn-primary',
+            buttonClass: 'btn btn-start',
             time: {
                 seconds: '00',
                 minutes: '0'
             },
+            secondOffset: 0,
             laps: []
         }
         this.toggleTimer = this.toggleTimer.bind(this);
@@ -28,8 +29,8 @@ class App extends Component {
             );
             this.setState({
                 timerOn: true,
-                buttonText: 'Stop Timer',
-                buttonClass: 'btn btn-warning'
+                buttonText: 'Pause Timer',
+                buttonClass: 'btn btn-stop'
             })
 
         } else {
@@ -37,7 +38,7 @@ class App extends Component {
             this.setState({
                 timerOn: false,
                 buttonText: 'Continue Timer',
-                buttonClass: 'btn btn-primary'
+                buttonClass: 'btn btn-start'
             })
         }
     }
@@ -45,27 +46,28 @@ class App extends Component {
     tick(){
         var currentSeconds = parseFloat(this.state.time['seconds']);
         var newSeconds = (parseInt(currentSeconds, 10) + 101).toString().substr(1);
+        var percentage = (newSeconds/60) * 100
         if(newSeconds === '60'){
             var currentMin = parseFloat(this.state.time['minutes']);
             this.setState(prevState => ({
                 time: {
                     seconds: '00',
                     minutes: currentMin + 1
-                }
+                },
+                secondOffset: percentage
             }))
         } else {
             this.setState(prevState => ({
                 time: {
                     ...prevState.time,
                     seconds: newSeconds
-                }
+                },
+                secondOffset: percentage
             }))
         }
         this.setState({
             timerDuration: this.state.timerDuration + 1
         })
-
-
     }
 
     lap(){
@@ -73,70 +75,79 @@ class App extends Component {
             id: this.state.laps.length + 1,
             time: this.state.time['minutes'] + ":" + this.state.time['seconds']
         }
+        var allLaps = this.state.laps.slice();
+        allLaps.unshift(newLap);
         this.setState({
-            laps: this.state.laps.concat(newLap)
-        });
+            laps: allLaps
+        })
     }
 
     resetWatch(){
         this.setState({
             timerDuration: 0,
             buttonText: 'Start Stop Watch',
-            buttonClass: 'btn btn-primary',
+            buttonClass: 'btn btn-start',
             time: {
                 seconds: '00',
                 minutes: '0'
             },
-            laps: []
+            laps: [],
+            secondOffset: 0
         })
     }
-
 
     render() {
         let { buttonText, buttonClass, laps } = this.state;
         let button;
         if( (this.state.timerOn === false) && (this.state.timerDuration > 0) ){
-            button = <button className="btn btn-danger" onClick={this.resetWatch}>Reset Stop Watch</button>;
+            button = <button className="btn btn-reset" onClick={this.resetWatch}>Reset Stop Watch</button>;
         } else if(this.state.timerOn === true){
-            button = <button className="btn btn-info" onClick={this.lap}>Lap</button>;
+            button = <button className="btn btn-lap" onClick={this.lap}>Lap</button>;
         }
-
+        const style = {
+          strokeDashoffset: 'calc(314.1592% * ('+this.state.secondOffset+' / 100))'
+        }
         return (
-            <div className="d-flex align-items-top h-100 bg-dark pt-5">
+            <div id="App">
                 <div className="container">
-                    <div className="row justify-content-center">
-                        <div className="col-8">
-                            <div className="card text-center">
-                                <h1 className="display-4">Stop Watch</h1>
-                                <h2 className="display-2">{this.state.time['minutes']}:{this.state.time['seconds']}</h2>
-                                <button className={buttonClass} onClick={this.toggleTimer} >{buttonText}</button>
-                                {button}
-                            </div>
+                    <div id="clock">
+                        <div className="secondsRadial" data-percentage="75">
+                            <svg viewport="0 0 2000 2000">
+                                <circle className="bars" r="50%" cx="50%" cy="50%"></circle>
+                                <circle
+                                    className="bars"
+                                    r="50%"
+                                    cx="50%"
+                                    cy="50%"
+                                    style={style}
+                                    ></circle>
+                            </svg>
                         </div>
+                        <h2 className="time">{this.state.time['minutes']}:{this.state.time['seconds']}</h2>
+                    </div>
+                    <div className="buttons">
+                        <button className={buttonClass} onClick={this.toggleTimer} >{buttonText}</button>
+                        {button}
                     </div>
                     {laps.length ? (
-                        <div className="row justify-content-center">
-                            <div className="col-8">
-                                <table id="lapTable" className="table table-striped table-dark">
-                                    <thead>
-                                        <tr>
-                                            <th>Lap</th>
-                                            <th>Time</th>
+                        <table id="lapTable">
+                            <thead>
+                                <tr>
+                                    <th>Lap</th>
+                                    <th>Time</th>
+                                </tr>
+                            </thead>
+                            <tbody className="border-bottom">
+                                {
+                                    this.state.laps.map(lap => {
+                                        return <tr key={lap.id}>
+                                            <td>{lap.id}</td>
+                                            <td>{lap.time}</td>
                                         </tr>
-                                    </thead>
-                                    <tbody className="border-bottom">
-                                        {
-                                            this.state.laps.reverse().map(lap => {
-                                                return <tr key={lap.id}>
-                                                    <td>{lap.id}</td>
-                                                    <td>{lap.time}</td>
-                                                </tr>
-                                            })
-                                        }
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+                                    })
+                                }
+                            </tbody>
+                        </table>
                     ):null}
                 </div>
             </div>
